be759648704e665cf104351d65fbc261ff9d7373
qiaoshoujiang <49113945+qiaoshoujiang@users.noreply.github.com>
2020-10-22 10:27:02 +0800

    Update 撸猫.js
    有点小问题，还没进入淘宝输入撸猫次数后就卡在当前界面了。我加了个进入撸猫界面的等待时间，大佬看看还可不可以改进一下。

--- 撸猫.js
+++ 撸猫.js
@@ -6,6 +6,10 @@ console.show();
 var h = device.height;
 var w = device.width;
 setScreenMetrics(w,h);
+log("正在打开淘宝");
+launch("com.taobao.taobao");
+log("请在10秒内进入撸猫界面");
+sleep(10000);
 var num = dialogs.input("输入撸猫次数", 300);
 log("开始撸猫");
 if(num==null){
